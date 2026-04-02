2758522e6fdf9ec1ff88afe77361c6efbacbb36f
magent <magent@cryptograss.live>
2026-04-02 23:21:37 +0000

    Disable MediaUploader tutorial step
    Self-explanatory upload flow doesn't need a tutorial page.

--- LocalSettings.php
+++ LocalSettings.php
@@ -210,8 +210,10 @@ $wgMSU_useDragDrop = true;
 $wgMSU_showAutoCat = true;
 
 # MediaUploader - step-by-step multi-file upload wizard
-# On production: requires running pickipedia-vps ansible to install the extension first
 wfLoadExtension( 'MediaUploader' );
+$wgMediaUploaderConfig = [
+    'tutorial' => [ 'enabled' => false ],
+];
 $wgMSU_checkAutoCat = true;
 $wgMSU_imgParams = '400px';
 $wgMSU_uploadsize = '1024mb';
