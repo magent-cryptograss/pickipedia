9e5dc8592614cba018df9fd7eff74ecab784b96c
magent <magent@cryptograss.live>
2026-04-03 22:10:43 +0000

    Disable MediaUploader nav until Caddy bug resolved (pickipedia#71)

--- LocalSettings.php
+++ LocalSettings.php
@@ -214,7 +214,8 @@ wfLoadExtension( 'MediaUploader' );
 $wgMediaUploaderConfig = [
     'tutorial' => [ 'enabled' => false ],
 ];
-$wgUploadNavigationUrl = '/wiki/Special:MediaUploader';
+# Disabled until Caddy upload bug resolved (pickipedia#71)
+# $wgUploadNavigationUrl = '/wiki/Special:MediaUploader';
 $wgMSU_checkAutoCat = true;
 $wgMSU_imgParams = '400px';
 $wgMSU_uploadsize = '1024mb';
